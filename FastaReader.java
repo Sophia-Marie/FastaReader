6c6c2f566a10f65f18b41101b9fd77075a5ff6a3
Sophia-Marie <soffi-marie@hotmail.com>
2017-01-25 00:00:57 +0100

    changed membervariable to local main variable

--- FastaReader.java
+++ FastaReader.java
@@ -1,4 +1,3 @@
-
 package fastareader;
 
 import java.util.ArrayList;
@@ -8,53 +7,45 @@ import java.io.*;
  * @author Sophia and Julia
  */
 public class FastaReader {
-    
+
     final private static String FILE = "C:\\Users\\Sophia\\Documents\\NetBeansProjects\\FastaReader\\src\\fastareader\\uniprot-escherichia.fasta";
-    private static String currentLine;
-    
-    public Fasta getNextSequence(BufferedReader inputStream){
-        
+
+    public Fasta getNextSequence(BufferedReader inputStream, String currentLine) {
+
         Fasta fasta = new Fasta();
-        
-        try{
-            if (currentLine.startsWith(">")){
+
+        try {
+            if (currentLine.startsWith(">")) {
                 fasta.setHeader(currentLine);
             }
-            currentLine=inputStream.readLine();
-            while(false==currentLine.startsWith(">") && currentLine!=null){   
-                    fasta.setSequence(currentLine);
-                    currentLine = inputStream.readLine();
+            currentLine = inputStream.readLine();
+
+            while (!currentLine.startsWith(">")) {
+                fasta.setSequence(currentLine);
+                currentLine = inputStream.readLine();
             }
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error");
         }
-        System.out.println(fasta.getHeader());
-        System.out.println(fasta.getSequence());
-        
         return fasta;
     }
-    
+
     public static void main(String[] args) {
         try {
-        BufferedReader inputStream = new BufferedReader(new FileReader(FILE));
-        FastaReader fastareader= new FastaReader();
-//        Fasta fast = new Fasta();
-        ArrayList<Fasta> allFastaObjects = new ArrayList<Fasta>();
-        int index=0;
-        currentLine= inputStream.readLine();
-            while (currentLine!=null){
-                Fasta fast = new Fasta();
-                fast = fastareader.getNextSequence(inputStream);
+            BufferedReader inputStream = new BufferedReader(new FileReader(FILE));
+            FastaReader fastaReader = new FastaReader();
+            ArrayList<Fasta> allFastaObjects = new ArrayList<Fasta>();
+            int index = 0;
+            String currentLine = inputStream.readLine();
+            while (currentLine != null) {
+                Fasta fast = fastaReader.getNextSequence(inputStream, currentLine);
                 allFastaObjects.add(index, fast);
-                index +=1;
+                index += 1;
+
             }
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error, File not found!");
         }
-        
- 
-        
+
     }
 }
