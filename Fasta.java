fc4f050c0d875e3aa36046070db980ef11f05a26
Sophia-Marie <soffi-marie@hotmail.com>
2017-01-26 03:30:40 +0100

    final

--- Fasta.java
+++ Fasta.java
@@ -1,4 +1,3 @@
-
 package fastareader;
 
 /**
@@ -6,36 +5,37 @@ package fastareader;
  * @author Sophia Descho und Julia Meyer
  */
 public class Fasta {
-    
+
     private String header;
     private String sequence;
     private int seqlen;
-    
-    public Fasta(){
-        header="";
-        sequence="";
-        seqlen=0;
-        
+
+    public Fasta() {
+        header = "";
+        sequence = "";
+        seqlen = 0;
+
     }
-   public void setHeader(String header) {
-       this.header = header;
-   }
-   
-   public void setSequence(String sequence) {
-       this.sequence += sequence;
-   }
-   
-   public String getHeader() {
-       return header;
-   }
-   
-   public String getSequence() { 
-       return sequence; 
-   }
-   
-   public int getSeqlen() {
-       this.seqlen= this.sequence.length();
-       return seqlen;
-   }
-   
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence += sequence;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public int getSeqlen() {
+        this.seqlen = this.sequence.length();
+        return seqlen;
+    }
+
 }
